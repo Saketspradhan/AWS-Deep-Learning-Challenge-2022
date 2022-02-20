8e9212bbf4c7a731a9fcb92eac6a2764580c359b
Saket Pradhan <65219413+Saketspradhan@users.noreply.github.com>
2022-02-20 16:18:36 +0530

    Add files via upload

---
+++ AWS-DL-Challenge-model-training.py
@@ -0,0 +1,124 @@
+from habana_frameworks.tensorflow import load_habana_module
+load_habana_module()
+
+import itertools
+import os
+import random
+
+import cv2
+import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from IPython.display import Image, display
+from keras.layers import Activation
+from keras.layers.convolutional import Conv2D
+from keras.layers.core import Dense, Flatten
+from keras.metrics import categorical_crossentropy
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import (Activation, BatchNormalization, Conv2D,
+                                     Dense, Dropout, Flatten, MaxPooling2D,
+                                     SeparableConv2D)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+
+real = "Dataset/real_and_fake_face/training_fake"
+fake = "Dataset/real_and_fake_face/training_fake"
+datadir = "Dataset/real_and_fake_face"
+
+real_path = os.listdir(real)
+fake_path = os.listdir(fake)
+
+def load_img(path):
+    image = cv2.imread(path)
+    image = cv2.resize(image, (224, 224))
+    return image[...,::-1]
+
+
+categories = ["training_real" , "training_fake"]
+for category in categories:
+    path = os.path.join(datadir, category)
+    for img in os.listdir(path):
+        img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
+        break
+    break
+
+training_data = []
+IMG_SIZE = 224
+
+categories = ["training_real" , "training_fake"]
+def create_training_data():
+    for category in categories:
+        path = os.path.join(datadir, category)
+        class_num = categories.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_UNCHANGED)
+                new_array = cv2.resize(img_array,(IMG_SIZE,IMG_SIZE))
+                training_data.append([new_array,class_num])
+            except: pass
+create_training_data()
+
+training_data = np.array(training_data)
+np.random.shuffle(training_data)
+
+X = []
+y = []
+
+for features,label in training_data:
+    X.append(features)
+    y.append(label)
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+y = np.array(y)
+
+X = X/255.0
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+train_x = tf.keras.utils.normalize(X_train,axis=1)
+test_x = tf.keras.utils.normalize(X_test, axis=1)
+
+model = tf.keras.models.Sequential([
+            tf.keras.layers.Conv2D(64,(3,3),activation = 'relu',
+                            input_shape= X.shape[1:]),
+            tf.keras.layers.Conv2D(64,(3,3),activation = 'relu'),
+            tf.keras.layers.MaxPooling2D(2,2),
+            tf.keras.layers.Conv2D(64,(3,3),activation = 'relu'),
+            tf.keras.layers.Conv2D(64,(3,3),activation = 'relu'),
+            tf.keras.layers.MaxPooling2D(2,2),
+            tf.keras.layers.Dropout(0.25),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation=tf.nn.relu),
+            tf.keras.layers.Dense(2, activation=tf.nn.softmax)
+
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+vgg16_model = keras.applications.vgg16.VGG16()
+
+model = Sequential()
+for layer in vgg16_model.layers[:-1]: model.add(layer)
+for layer in model.layers: layer.trainable = False
+
+model.add(Dense(2, activation='softmax'))
+model.save('final_model')
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+def load_img(path):
+    image = cv2.resize(path, (224, 224))
+    return image[...,::-1]
+
+
+def prepare(image):
+    IMG_SIZE = 224
+    new_array = cv2.resize(image, (IMG_SIZE, IMG_SIZE)) 
+    return new_array.reshape(-1, IMG_SIZE,IMG_SIZE,3)
